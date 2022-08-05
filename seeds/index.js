4431103a51b2ddbab545d4ea4d51bef38767abe6
Jeffrey <wzhang@wzhangs-MBP.attlocal.net>
2022-08-05 14:16:12 -0700

    seeded Department and Doctor database with sequelize

--- seeds/index.js
+++ seeds/index.js
@@ -1,21 +1,6 @@
-// const sequelize = require('../config/connection');
-// const seedDepartments = require('./department-seeds');
-// const seedDoctors = require('./doctor-seeds');
-
-// const seedAll = async () => {
-//   await sequelize.sync({ force: true });
-
-//   await seedDepartments();
-
-//   await seedDoctors();
-
-//   process.exit(0);
-// };
-
-// seedAll();
 
 const sequelize = require('../config/connection');
-const { Appointment, Department, Doctor, Patient } = require('../models');
+const { Department, Doctor} = require('../models');
 
 const departmentData = require('./departmentData.json');
 const doctorData = require('./doctorData.json');
@@ -23,17 +8,8 @@ const doctorData = require('./doctorData.json');
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  const departments = await Department.bulkCreate(departmentData, {
-    individualHooks: true,
-    returning: true,
-  });
-
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
+  const departments = await Department.bulkCreate(departmentData);
+  const doctors = await Doctor.bulkCreate(doctorData);
 
   process.exit(0);
 };
