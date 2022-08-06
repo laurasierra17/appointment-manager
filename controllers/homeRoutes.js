2a7d77a6b963e0b907ffcbeb8cc4c7f12df57095
laurasierra17 <laura.sierra17@gmail.com>
2022-08-06 19:32:03 -0400

    format profile page based on info from database

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,26 +1,30 @@
 const router = require('express').Router();
-const { Patient, Appointment } = require('../models')
+const { Patient, Appointment, Department, Doctor } = require('../models')
 
 // Render profile page of logged in patient
-router.get('/profile/:id' , async (req, res) => {
+router.get('/profile/:id', async (req, res) => {
     try {
         // Find patient based on user id stored in session
-        const patientData = await Patient.findByPk(1, {
-            include: {
+        const patientData = await Patient.findByPk(req.session.user_id, {
+            include:
+            {
                 model: Appointment,
                 where: {
-                    patient_id: 1
+                    patient_id: req.session.user_id
                 }
             }
         });
-
         // Parse for readability
-        // const patient = patientData.get({ plain: true });
-        console.log("patientdata", patientData);
+        const patient = patientData.get({ plain: true });
 
-        // res.render('profile');
-        res.json(patientData)
-        // res.render('profile', { ...patientData })
+        // Get the Doctor's name associated to the Appointment
+        const doctorData = await Doctor.findByPk(patient.appointment.doctor_id, {
+            attributes: ['last_name']
+        });
+        const doctor = doctorData.get({ plain: true });
+
+        // Render profile page with information from the database
+        res.render('profile', { ...patient, doctor })
     } catch (err) {
         console.log(err)
         res.status(500).json(err);
