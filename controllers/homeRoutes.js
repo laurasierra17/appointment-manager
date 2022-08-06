8ff6f8052288c9dfbf18821440c0580a0cc19244
laurasierra17 <laura.sierra17@gmail.com>
2022-08-06 19:10:24 -0400

    fix association between patient and appointment; appointment and doctor

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -5,17 +5,24 @@ const { Patient, Appointment } = require('../models')
 router.get('/profile/:id' , async (req, res) => {
     try {
         // Find patient based on user id stored in session
-        const patientData = await Patient.findByPk(req.session.patient_id, {
+        const patientData = await Patient.findByPk(1, {
             include: {
-                model: Appointment
+                model: Appointment,
+                where: {
+                    patient_id: 1
+                }
             }
         });
-        // Parse for readability
-        patientData = patientData.get({ plain: true });
 
-        res.render('profile');
+        // Parse for readability
+        // const patient = patientData.get({ plain: true });
+        console.log("patientdata", patientData);
+
+        // res.render('profile');
+        res.json(patientData)
         // res.render('profile', { ...patientData })
     } catch (err) {
+        console.log(err)
         res.status(500).json(err);
     }
 });
--- models/index.js
+++ models/index.js
@@ -3,11 +3,18 @@ const Doctor = require('./Doctor');
 const Patient = require('./Patient');
 const Department = require('./Department');
 
+// Associations between Appointment and Patient
+Appointment.hasMany(Patient, { foreignKey: 'patient_id' });
+Patient.belongsTo(Appointment, { foreignKey: 'id' });
+
+// // Associations between Appointment and Doctor
+Appointment.hasMany(Doctor, { foreignKey: 'doctor_id' });
+Doctor.belongsTo(Appointment, { foreignKey: 'id' });
 // Association between Doctor and Patient through Appointment
-Doctor.belongsToMany(Patient, {through: Appointment, foreignKey: 'doctor_id'});
-Doctor.hasMany(Patient, {foreignKey: 'patient_id'});
-Patient.belongsToMany(Doctor, {through: Appointment, foreignKey: 'patient_id'});
-Patient.hasMany(Doctor, {foreignKey: 'doctor_id'});
+// Doctor.belongsToMany(Patient, { through: Appointment });
+// Doctor.hasMany(Patient, {foreignKey: 'patient_id'});
+// Patient.belongsToMany(Doctor, { through: Appointment });
+// Patient.hasMany(Doctor, {foreignKey: 'doctor_id'});
 
 // Association between Doctor and Department
 Department.hasMany(Doctor);
