099e989493ebadbd3e030a423d194e701577e479
Liam McCarthy <ltmccarthy9@gmail.com>
2022-08-05 19:10:09 -0500

    script fetch login/signup

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,33 @@
+const loginButton = $('loginbtn');
+const USERNAME = $('username');
+const PASSWORD = $('password');
+const signupButton = $('signupbtn');
 // set up some routes and then we can connect them to the event listeners
-
-
+// possibly make this a middleware function 
+$(loginButton).click(() => {
+    if(USERNAME && PASSWORD) {
+        fetch(`/api/patient/${USERNAME}`, {
+            method: "GET",
+            body: JSON.stringify({
+              username: USERNAME,
+              password: PASSWORD  
+            })
+        })
+    }
+})
 
 // LOGIN PAGE < - - - - - - - - 
-
+$(signupButton).click(() => {
+    if(USERNAME && PASSWORD) {
+        fetch(`/api/patient/${USERNAME}`, {
+            method: "POST",
+            body: JSON.stringify({
+                username: USERNAME,
+                password: PASSWORD
+            })
+        })
+    }
+})
 // when we log in
 // const 
 //fetch(/api/email)
@@ -34,3 +58,4 @@
 // DOCTOR PAGE/CREATE APPOINTMENT < - - - - - - - - 
 // const
 // fetch(doctors/appointements)
+
