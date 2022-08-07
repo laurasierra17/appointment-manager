77a56efcbd891798d9990754689a0619d74495a0
Jeffrey <wzhang@wzhangs-MBP.attlocal.net>
2022-08-06 19:57:47 -0700

    added get by id department route

--- controllers/api/departmentRoutes.js
+++ controllers/api/departmentRoutes.js
@@ -13,7 +13,24 @@ router.get('/', async (req, res) => {
     }
 });
 
-
+router.get('/:id', async (req, res) => {
+    try {
+        // Get all departments
+        const dptData = await Department.findAll();
+    
+        // Serialize data so the template can read it
+        const departments = dptData.map((data) => data.get({ plain: true }));
+    
+        // Pass serialized data and session flag into template
+        res.render('appointment', { 
+            departments, 
+        //   logged_in: req.session.logged_in 
+        });
+        } catch (err) {
+        res.status(500).json(err);
+        }
+   
+});
 
 
 
