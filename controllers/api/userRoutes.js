65927f8eaa5f51ece9b63328ba8088b7f27fc356
laurasierra17 <laura.sierra17@gmail.com>
2022-08-07 22:18:36 -0400

    logout functionality

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -48,6 +48,16 @@ router.post('/', async (req, res) => {
     }
 });
 
+// Logout
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+})
 
 
 module.exports = router;
